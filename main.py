8de179ef2d1bfe98fc30698156a24f59b2707763
NjokiNdichu <rejoicenjoki0@gmail.com>
2023-04-27 15:05:26 +0300

    psycopg2

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+
 from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
 
+try:
+    conn = psycopg2.connect("dbname='myduka' user='postgres' host='localhost' port='5433' password='njokin2023#'")
+    print('connected successfuly')
+except Exception as e:
+    print ("I am unable to connect to the database", e)
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    username='Rejoice Njoki'
+    return render_template('index.html', username=username)
 
 
 @app.route('/products')
 def products():
-    return render_template('products.html')
+    
+
+    cur = conn.cursor()
+    cur.execute("SELECT * from products;")
+    rows = cur.fetchall()
+    print (rows)
+    return render_template('products.html', rows=rows)
 
 app.run()
